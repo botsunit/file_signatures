b32313bbf9ebf6b539dd73a13637785f95d23bd5
glejeune <gregoire.lejeune@free.fr>
2017-02-28 15:17:23 +0100

    Bump to 0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule File.Signatures.Mixfile do
   def project do
     [
       app: :file_signatures,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
